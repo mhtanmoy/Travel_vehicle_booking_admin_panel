565e5c60689a645252cc12849c14b201be669e76
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-11-29 22:54:04 +0600

    before adding side bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
-      <h1>Working on proccess..</h1>
+      <Header/>
+        <main className="py-3">
+          <Container>
+          <h1>Working on proccess..</h1>
+          </Container>
+        </main>
+      <Footer />
     </div>
   );
 }
