f684b80414146f500a4a92a5782f215d1e7aa238
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-11-30 23:07:17 +0600

    added sidebar with header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import logo from '../assets/logo/Logo.png';
 
-function Header() {
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
 
-export default Header
+const Header = (props) => {
+    const [inactive, SetInactive] = useState(false);
+
+    return <div className={`side-menu ${inactive ? "inactive" : ""}`}>
+            <div className="top-section">
+                <div className="logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div onClick={ () => {
+                    SetInactive(!inactive);
+                }} className="toggle-button">
+                    <i class="bi bi-plus-square"></i>
+                </div>
+            </div>
+
+            <div className="divider">
+
+            </div>
+        </div>;
+    
+};
+
+export default Header;
+
