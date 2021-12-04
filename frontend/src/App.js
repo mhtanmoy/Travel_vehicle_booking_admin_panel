96b1d1133f2a145b6fdf7bf1bd9ac241a378ba82
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-12-04 19:29:24 +0600

    conect screen to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,14 @@ import {
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screen/HomeScreen'
+import AdminScreen from './screen/AdminScreen'
+import BookingHistoryScreen from './screen/BookingHistoryScreen'
+import CustomerScreen from './screen/CustomerScreen'
+import DriAccVerScreen from './screen/DriAccVerScreen'
+import DriverScreen from './screen/DriverScreen'
+import EditContactScreen from './screen/EditContactScreen'
+import ManageBookingScreen from './screen/ManageBookingScreen'
+import VehiclesScreen from './screen/VehiclesScreen'
 
 
 function App() {
@@ -23,6 +31,14 @@ function App() {
           <div className={`homeSwitch ${inactive ? "inactive" : ""}`}>
             <Switch>
               <Route path={'/'} component={HomeScreen} exact/>
+              <Route path={'/users'} component={AdminScreen}/>
+              <Route path={'/aftercompleted'} component={BookingHistoryScreen}/>
+              <Route path={'/customeruser'} component={CustomerScreen}/>
+              <Route path={'/driversverify'} component={DriAccVerScreen}/>
+              <Route path={'/driver'} component={DriverScreen}/>
+              <Route path={'/editcontact'} component={EditContactScreen}/>
+              <Route path={'/bookingrequest'} component={ManageBookingScreen}/>
+              <Route path={'/vehicles'} component={VehiclesScreen}/>
             </Switch>
           </div>
         </main>
