1f6c240a3951c8f50086d0217d70ff393910f28c
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-12-14 21:14:29 +0600

    added reducer, constants, action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,9 +5,19 @@ import {
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+import { 
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userUpdateProfileReducer 
+} from './reducers/userReducers'
+
 
 const reducer = combineReducers({
-    
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 })
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ?
